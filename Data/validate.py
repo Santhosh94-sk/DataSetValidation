d7030220cc18b2f0f5a56fae1bc1f59b7f621a6c
Santhosh94-sk <88767055+Santhosh94-sk@users.noreply.github.com>
2023-03-18 18:57:04 +0530

    Create validate.py
    Created a sample data validation script with pandas

---
+++ Data/validate.py
@@ -0,0 +1,9 @@
+import pandas as pd
+data=pd.read_csv("data/my_dataser.csv")
+rule1=data['column1'].isnull().sum()==0
+rule2=data['column2'].astype(str).str.isnumeric().all()
+rule3=data(['column3']>=0).all() and (data['column3']<=100.all()
+if rule1 and rule2 and rule3:
+                                      print("Validation Passed!")
+else:
+                                      print("Validation Failed")
